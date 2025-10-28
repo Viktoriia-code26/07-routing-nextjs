f5b2b0b34c279d62180275deb4bf5ad8fb6f0223
Viktoriia P. <pohrebniakvika@gmail.com>
2025-10-28 22:38:53 +0100

    included modified app/notes/layout.tsx

--- app/notes/layout.tsx
+++ app/notes/layout.tsx
@@ -1,25 +1,25 @@
 // app/notes/layout.tsx
 import { ReactNode } from "react";
-import SidebarNotes from "./filter/@sidebar/default"; 
-import css from "./layoutNotes.module.css"; 
+import SidebarNotes from "./filter/@sidebar/default";
+import css from "./layoutNotes.module.css";
 
 interface Props {
   children: ReactNode;
-  params: { tag?: string | string[] }; 
+  params: Promise<{ tag?: string | string[] }>;
 }
 
-export default function FilterLayout({ children, params }: Props) {
-  
-  const currentTag = Array.isArray(params?.tag) ? params.tag[0] : params?.tag ?? "";
+export default async function FilterLayout({ children, params }: Props) {
+  const resolvedParams = await params;
+  const currentTag = Array.isArray(resolvedParams?.tag)
+    ? resolvedParams.tag[0]
+    : resolvedParams?.tag ?? "";
 
   return (
     <div className={css.container}>
       <aside className={css.sidebar}>
         <SidebarNotes currentTag={currentTag} />
       </aside>
-      <main className={css.notesWrapper}>
-        {children}
-      </main>
+      <main className={css.notesWrapper}>{children}</main>
     </div>
-  )
-};
+  );
+}
