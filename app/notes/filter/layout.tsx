b027c83f37549d241f14563f9c9725a31d50598f
Viktoriia P. <pohrebniakvika@gmail.com>
2025-10-30 20:14:32 +0100

    all files included

--- app/notes/filter/layout.tsx
+++ app/notes/filter/layout.tsx
@@ -1,24 +1,20 @@
 import { ReactNode } from "react";
 import SidebarNotes from "./@sidebar/default";
-import css from "../layoutNotes.module.css"; 
+import css from "../layoutNotes.module.css";
 
 interface Props {
   children: ReactNode;
   modal: ReactNode;
-  params: Promise<{ tag?: string | string[] }>;
+  params: { tag?: string | string[] }; // ✅ без Promise
 }
 
-export default async function FilterLayout({ children, modal, params }: Props) {
-
-  const resolvedParams = await params;
-
-  const currentTag = Array.isArray(resolvedParams?.tag)
-    ? resolvedParams.tag[0]
-    : resolvedParams?.tag ?? "all";
+export default function FilterLayout({ children, modal, params }: Props) {
+  const currentTag = Array.isArray(params?.tag)
+    ? params.tag[0]
+    : params?.tag ?? "";
 
   return (
     <div className={css.container}>
-
       <aside className={css.sidebar}>
         <SidebarNotes currentTag={currentTag} />
       </aside>
