dda02b6b89c1b1fe33c4ec54296182a61669ba39
Viktoriia P. <pohrebniakvika@gmail.com>
2025-10-31 10:00:48 +0100

    all files included

--- app/notes/filter/@sidebar/default.tsx
+++ app/notes/filter/@sidebar/default.tsx
@@ -1,19 +1,23 @@
+"use client";
+
 import css from "../../../sidebarNotes.module.css";
 import Link from "next/link";
 
 const TAGS = ["Work", "Personal", "Meeting", "Shopping", "Todo"];
 
 interface SidebarNotesProps {
-  currentTag: string;
+  currentTag?: string;
 }
 
-export default function SidebarNotes({ currentTag }: SidebarNotesProps) {
+export default function SidebarNotes({ currentTag = "all" }: SidebarNotesProps) {
+  const normalizedTag = (currentTag ?? "").toLowerCase();
+
   return (
     <ul className={css.menuList}>
       <li className={css.menuItem}>
         <Link
-          href={`/notes/filter/all`}
-          className={`${css.menuLink} ${currentTag === "all" ? css.active : ""}`}
+          href="/notes/filter"
+          className={`${css.menuLink} ${normalizedTag === "all" ? css.active : ""}`}
         >
           All notes
         </Link>
@@ -22,14 +26,10 @@ export default function SidebarNotes({ currentTag }: SidebarNotesProps) {
       {TAGS.map(tag => (
         <li
           key={tag}
-          className={`${css.menuItem} ${
-            currentTag.toLowerCase() === tag.toLowerCase() ? css.active : ""
-          }`}
+          className={`${css.menuItem} ${normalizedTag === tag.toLowerCase() ? css.active : ""
+            }`}
         >
-          <Link
-            href={`/notes/filter/${tag}`}
-            className={css.menuLink}
-          >
+          <Link href={`/notes/filter/${tag}`} className={css.menuLink}>
             {tag}
           </Link>
         </li>
